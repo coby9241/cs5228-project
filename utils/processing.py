c02eb333e30fe23431caa969a2fd1dc8a06d3e72
darren.ongwx <darren.ongwx@shopee.com>
2022-10-02 21:04:21 +0800

    eda

---
+++ utils/processing.py
@@ -0,0 +1,118 @@
+import numpy as np
+import pandas as pd
+
+from utils.config import *
+
+def convert_to_lowercase(df, columns):
+    for col in columns:
+        df[col] = df[col].str.lower()
+    
+    return df
+
+
+def preprocess_property_type(df):
+    '''
+    1. Standardise all casing to lower case
+    2. One-hot encode property type to public/hybrid/private type
+    '''
+    df = convert_to_lowercase(df, convert_to_lowercase_cols)
+    df['property_type'].replace(public_housing_property_type, 'property_type_public', inplace=True)
+    df['property_type'].replace(private_housing_property_type, 'property_type_private', inplace=True)
+    df['property_type'].replace(hybrid_housing_property_type, 'property_type_hybrid', inplace=True)
+    df = pd.merge(df, pd.get_dummies(df['property_type'], drop_first=True), left_index=True, right_index=True)
+    
+    return df
+
+
+def preprocess_tenure(df):
+    '''
+    1. Impute missing value with NA
+    2. One-hot encode tenure to low, high, freehold, NA type
+    '''
+    df['tenure'] = df['tenure'].replace({np.nan: 'NA'})
+    df['tenure'].replace(tenure_low_year, 'tenure_low_year', inplace=True)
+    df['tenure'].replace(tenure_high_year, 'tenure_high_year', inplace=True)
+    df = pd.merge(df, pd.get_dummies(df['tenure'], drop_first=True), left_index=True, right_index=True)
+    
+    return df
+
+
+def preprocess_num_beds(df):
+    '''
+    Remove missing values
+    '''
+    df = df[df['num_beds'].notna()]
+    
+    return df 
+
+
+def preprocess_num_baths(df):
+    '''
+    Fill missing values with median num_baths of corresponding num_beds values
+    '''
+    median_num_baths = df.groupby('num_beds').agg({'num_baths':'median'}).rename(columns={'num_baths': 'median_num_baths'}).reset_index()
+    df = pd.merge(df, median_num_baths, on='num_beds', how='left')
+    df['num_baths'] = np.where(df['num_baths'].isnull(),df['median_num_baths'],df['num_baths'])
+    df.drop(columns='median_num_baths', inplace=True)
+    
+    return df 
+
+
+def preprocess_size_sqft(df):
+    '''
+    Remove outliers:
+    1. Remove if sqft = 0
+    2. Remove if sqft >= 70000  
+    '''
+    df = df[(df['size_sqft'] > 0) & (df['size_sqft'] < 70000)]
+    
+    return df 
+
+
+def preprocess_floor_level(df):
+    '''
+    To fill in
+    '''
+    
+    return df 
+
+
+def preprocess_furnishing(df):
+    '''
+    1. Group 'na' and 'unspecified' values to same category
+    2. One-hot encode furnishing to fully, partial, unfurnished, unspecified
+    '''
+    df['furnishing'].replace({'na': 'unspecified'}, inplace=True)
+    df_ohe = pd.get_dummies(df['furnishing'], drop_first=True)
+    renamed_cols = ['furnishing_' + x for x in df_ohe.columns]
+    df_ohe.columns = renamed_cols
+    df = pd.merge(df, df_ohe, left_index=True, right_index=True)
+    
+    return df
+
+
+def preprocess_available_unit_types(df):
+    '''
+    
+    '''
+    
+    return df 
+
+
+def preprocess(df):
+    df = preprocess_property_type(df)
+    df = preprocess_tenure(df)
+    df = preprocess_num_beds(df)
+    df = preprocess_num_baths(df)
+    df = preprocess_size_sqft(df)
+    df = preprocess_floor_level(df)
+    df = preprocess_furnishing(df)
+    
+    return df
+
+
+if __name__ == '__main__':
+    df = pd.read_csv('data/train.csv')
+    df = preprocess(df)
+
+    print(df[:10])
---
+++ utils/visualisation.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+    
+    
+def plot_boxplot(df, feature, y='price', y_min=-np.inf, y_max=np.inf, figsize=(15, 3), whis=1.5):
+    '''
+    df: dataframe containing predictors and target variable
+    feature: name of feature
+    y: target variable. Default value: price
+    y_min: min threshold of target variable to be plotted on box plot for better visualisation. Default value: - infinity
+    y_max: max threshold of target variable to be plotted on box plot for better visualisation. Default value: infinity
+    figsize: figure size of box plot. Default value: (15, 3) 
+    whis: whis of boxplot. Can pass float or (float, float). E.g. if (0.05, 0.95) means whis is at 5th and 95th percentile. Default value: 1.5
+    '''
+    fig, ax = plt.subplots(figsize=figsize)
+
+    # Filter outliers in y 
+    dataset = df.copy(deep=True)
+    dataset = dataset[(dataset[y] > y_min) & (dataset[y] < y_max)]
+
+    row = []
+    categories = sorted(df[feature].drop_duplicates().tolist())
+    for cat in categories: 
+        row.append(dataset[dataset[feature]==cat][y].tolist())
+
+    pos = np.array(range(len(row))) + 1
+    ax.boxplot(row, positions=pos, vert=False, whis=whis)
+    ax.set_yticklabels(categories)
+    
+    plt.ylabel(y)
+    plt.xlabel(feature)
+    plt.title(f'{y.upper()} distribution (Box Plot) across {feature.upper()}')
+    plt.show()
+    
+    
+def plot_scatterplot(df, X, y, y_min=-np.inf, y_max=np.inf, figsize=(15,10)):
+    '''
+    df: dataframe containing predictors and target variable
+    X: x variable
+    y: y variable
+    y_min: min threshold of y variable to be plotted on box plot for better visualisation. Default value: - infinity
+    y_max: max threshold of y variable to be plotted on box plot for better visualisation. Default value: infinity
+    figsize: figure size of box plot. Default value: (15, 10) 
+    '''
+    ax = df[(df[y] > y_min) & (df[y] < y_max)].plot(
+        kind="scatter", 
+        x=X, 
+        y=y, 
+        figsize=figsize,
+        alpha=0.4,
+    )
+    
+    # add axis labels
+    plt.ylabel(y)
+    plt.xlabel(X)
+    plt.title(f'{y.upper()} vs {X.upper()}')
+
+    plt.show()
